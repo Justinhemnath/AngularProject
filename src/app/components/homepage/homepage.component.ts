0b8661670de69f86a2cb2ff6552c2b32a26606a1
Hemnath Balasubramanian <hemnath@theproindia.com>
2023-01-26 18:18:35 +0530

    26/1

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -9,12 +9,15 @@ import { Component } from '@angular/core';
 export class HomepageComponent {
   value: number = 0
   height: string = "4em"
+  triggerText: string = "COOL"
 
   valChanger() {
     this.value += 1
+    this.triggerText = this.value > 5 ? "WARNING: EXCEEDED" : "COOL"
   }
   
   heightChanger() {
     this.height = this.height == "4em" ? "10em" : "4em" 
   }
+
 }
